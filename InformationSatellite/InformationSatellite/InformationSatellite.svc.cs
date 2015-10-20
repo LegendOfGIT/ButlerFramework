7dd9bfce75b80ff4c83eb73259f5ce774f1874cd
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2015-10-20 20:20:21 +0200

    -

--- InformationSatellite/InformationSatellite/InformationSatellite.svc.cs
+++ InformationSatellite/InformationSatellite/InformationSatellite.svc.cs
@@ -1,19 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 
-using Butler.Shared;
-
 namespace Infosatellite
 {
-    // HINWEIS: Mit dem Befehl "Umbenennen" im Menü "Umgestalten" können Sie den Klassennamen "Service1" sowohl im Code als auch in der SVC- und der Konfigurationsdatei ändern.
     public class Infosattelite : IInfosatellite
     {
         public void Process(string template, Dictionary<string, string> parameters)
         {
             new Task(
                 () => {
-                    new Kernel {
-                    }.Process(
+                    Kernel.Process(
                         template, 
                         parameters
                     );
