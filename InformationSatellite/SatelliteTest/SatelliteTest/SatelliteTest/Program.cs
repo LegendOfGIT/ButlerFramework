6a73e65867b35c1a7686775073e9754a7aa2bf78
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2015-10-20 20:45:44 +0200

    -

---
+++ InformationSatellite/SatelliteTest/SatelliteTest/SatelliteTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+
+namespace SatelliteTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var template = File.ReadAllText(@"..\..\App_Data\gelbeseiten.crawl");
+
+            var client = new InformationSatellite.InfosatelliteClient();
+            client.Process(template, null);
+        }
+    }
+}
