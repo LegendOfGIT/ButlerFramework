0aafc6d2f71c0a2399f6e3a64e4a40d78e7053b4
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2015-10-29 23:21:23 +0100

    - Crawling-Template für mytoys.de erstellt. - Warehouse-Mock für Tests aufgebaut.

---
+++ InformationSatellite/SatelliteTest/SatelliteTest/ProviderMock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Data.Warehouse;
+
+namespace SatelliteTest
+{
+    public class ProviderMock : StorageProvider
+    {
+        public IEnumerable<Dictionary<string, IEnumerable<string>>> DigInformation(string question)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StoreInformation(Dictionary<string, IEnumerable<string>> information)
+        {
+            information = information.ToDictionary(entry => entry.Key, entry => entry.Value.Select(e => new string(e.Where(c => !char.IsControl(c)).ToArray()).Trim()));
+        }
+    }
+}
