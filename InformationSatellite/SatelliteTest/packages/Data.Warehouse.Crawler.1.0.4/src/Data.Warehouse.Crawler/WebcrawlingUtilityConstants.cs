88af776eb320a93aed6503ebd91682a295866d33
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2015-11-05 20:15:20 +0100

    -

---
+++ InformationSatellite/SatelliteTest/packages/Data.Warehouse.Crawler.1.0.4/src/Data.Warehouse.Crawler/WebcrawlingUtilityConstants.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Data.Warehouse.Crawler
+{
+    public class WebcrawlingUtilityConstants
+    {
+        public const string BaseUri = "base.uri";
+        public const string CommandBrowse = "browse()";
+        public const string CurrentUri = "current.uri";
+
+        public static List<string> Commands = new List<string>
+        {
+            CommandBrowse
+        };
+        public static List<string> BasicVariables = new List<string>
+        {
+            BaseUri,
+            CurrentUri
+        };
+    }
+}
