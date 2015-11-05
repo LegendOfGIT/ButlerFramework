fc3f816a51a0f1b07c252fdbb6458442e36014f0
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2015-11-05 20:10:29 +0100

    -

---
+++ InformationWarehouse/InformationWarehouse/packages/Data.Mining.1.0.0/src/Data.Mining/MiningQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Data.Mining
+{
+    public class MiningQuery
+    {
+        public string Target { get; set; }
+        public IEnumerable<string> Expressions { get; set; }
+
+        public MiningQuery Query { get; set; }
+    }
+}
