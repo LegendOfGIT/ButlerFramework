ae1e72a96c19b430e69a00b1e84688c1cc94e40f
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2015-11-09 06:39:26 +0100

    - Suchvorlage für mytoys.de erweitert. (Farbe, Kleidungsgröße)

---
+++ InformationSatellite/SatelliteTest/packages/Data.Warehouse.Crawler.1.0.10/src/Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Data.Warehouse.Crawler;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var templatesfiles = new[]
+            {
+                @"G:\Entwicklung\GitHUB\ButlerFramework\InformationSatellite\SatelliteTest\SatelliteTest\App_Data\shopping\mytoys.crawl",
+                //@"..\..\App_Data\chefkoch.crawl",
+                //@"..\..\App_Data\stackoverflow.crawl",
+                //@"..\..\App_Data\fun\9gag.crawl"
+            };
+            templatesfiles.ToList().ForEach(templatefile =>
+            {
+                var template = File.ReadAllText(templatefile);
+
+                var compiler = new WebcrawlerCompiler();
+                var commandset = compiler.ParseCommandset(Regex.Split(template, Environment.NewLine));
+            });
+        }
+    }
+}
