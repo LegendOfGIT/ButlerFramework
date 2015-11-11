017ecf3c1ec116c6dd3c2d1677ce84fa1896d4a1
Marcel Schneider <Marcel.Schneider1@googlemail.com>
2015-11-11 06:26:51 +0100

    - Konsolenprogramm für Warehousetest hinzugefügt.

---
+++ InformationWarehouse/WarehouseTest/WarehouseTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new InformationWarehouse.InformationWarehouseClient();
+            var information = client.DigInformation("Kleidung bis 20 EUR");
+        }
+    }
+}
