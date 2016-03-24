eb2d7ef1ee610a4ee1f893067dd77825ecb057cd
Marcel Schneider <Marcel.Schneider@arvato.com>
2016-03-24 17:08:11 +0100

    -

--- ButlerSite/ButlerSite/Controllers/HomeController.cs
+++ ButlerSite/ButlerSite/Controllers/HomeController.cs
@@ -2,70 +2,78 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
-using ButlerSite.InformationWarehouse;
+using ButlerSite.DataWarehouse;
 using ButlerSite.Models;
 
 namespace ButlerSite.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
-        public ActionResult Search(string question)
-        {
-            var actionresult = default(ActionResult);
-
-            if(!string.IsNullOrEmpty(question))
-            {
-                var information = new List<InformationViewModel>();
-
-                var key = default(string);
-                var subinformation = default(IEnumerable<object>);
-
-                var warehouseclient = new InformationWarehouseClient();
-                var diggingresult = warehouseclient.DigInformation(question);
-                foreach(var item in diggingresult)
-                {
-                    var informationitem = new InformationViewModel();
-
-                    //  Bilder
-                    key = "image";
-                    subinformation = item.ContainsKey(key) ? item[key] : default(IEnumerable<object>);
-                    if(subinformation != null && subinformation.Any())
-                    {
-                        informationitem.Images = subinformation.Where(info => !string.IsNullOrEmpty(info.ToString())).Select(info => new Uri(info.ToString()));
-                    }
-
-                    information.Add(informationitem);
-                }
-
-                actionresult = View(information);
-            }
-            else
-            { 
-                ViewBag.Message = "Your search page.";
-
-                actionresult = View();
-            }
-
-            return actionresult;
-        }
+      return View();
     }
+
+    public ActionResult About()
+    {
+      ViewBag.Message = "Your application description page.";
+
+      return View();
+    }
+
+    public ActionResult Contact()
+    {
+      ViewBag.Message = "Your contact page.";
+
+      return View();
+    }
+
+    public ActionResult Search(string question)
+    {
+      var actionresult = default(ActionResult);
+
+      if (!string.IsNullOrEmpty(question))
+      {
+        var information = new List<InformationViewModel>();
+
+        var key = default(string);
+        var subinformation = default(IEnumerable<object>);
+
+        var warehouseclient = new DataWarehouseClient();
+        var diggingresult = warehouseclient.DigInformation(question);
+        foreach (var item in diggingresult)
+        {
+          var informationitem = new InformationViewModel();
+
+          //  Bilder
+          key = "image";
+          subinformation = item.ContainsKey(key) ? item[key] : default(IEnumerable<object>);
+          if (subinformation != null && subinformation.Any())
+          {
+            informationitem.Images = subinformation.Where(info => !string.IsNullOrEmpty(info.ToString())).Select(info => new Uri(info.ToString()));
+          }
+
+          //  Uri
+          key = "uri";
+          subinformation = item.ContainsKey(key) ? item[key] : default(IEnumerable<object>);
+          if (subinformation != null && subinformation.Any())
+          {
+            informationitem.Uri = subinformation.Select(info => new Uri(info.ToString())).FirstOrDefault();
+          }
+
+          information.Add(informationitem);
+        }
+
+        actionresult = View(information);
+      }
+      else
+      {
+        ViewBag.Message = "Your search page.";
+
+        actionresult = View();
+      }
+
+      return actionresult;
+    }
+  }
 }
--- ButlerSite/ButlerSite/Models/SearchViewModels.cs
+++ ButlerSite/ButlerSite/Models/SearchViewModels.cs
@@ -4,9 +4,12 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ButlerSite.Models
 {
-    public class InformationViewModel
-    {
-        [Display(Name = "Images")]
-        public IEnumerable<Uri> Images { get; set; }
-    }
+  public class InformationViewModel
+  {
+    [Display(Name = "Images")]
+    public IEnumerable<Uri> Images { get; set; }
+    
+    [Display(Name = "Uri")]
+    public Uri Uri { get; set; }
+  }
 }
